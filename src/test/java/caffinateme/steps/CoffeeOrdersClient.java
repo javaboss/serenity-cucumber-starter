6beb8fc7b3b8b0e57a2b49f0d9b3dee6ff616848
Neil Liddicott <javaboss@mail.com>
2021-01-06 14:37:33 +0000

    Partial implementation

--- src/test/java/caffinateme/steps/CoffeeOrdersClient.java
+++ src/test/java/caffinateme/steps/CoffeeOrdersClient.java
@@ -1,6 +1,7 @@
 package caffinateme.steps;
 
 import caffinateme.OrderReceipt;
+import net.thucydides.core.annotations.Step;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,16 +10,19 @@ public class CoffeeOrdersClient {
 
     List<Order> orders = new ArrayList<>();
 
+    @Step
     public OrderReceipt placeOrder(long customerId, int quantity, String product) {
         Order order = new Order(customerId, quantity, product);
         orders.add(order);
         return order.getReceipt();
     }
 
+    @Step
     public List<Order> getOrders() {
         return new ArrayList<>(orders);
     }
 
+    @Step
     public void updateCustomerEta(long customerId, int minutesAway) {
         orders.stream().filter(order -> order.getCustomerId() == customerId)
                 .forEach(order -> order.updateETATo(minutesAway));
